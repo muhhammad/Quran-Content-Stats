c20b59fe370414d116e0f378f7d5b8e6ecb88a58
Mohamed Shameem <muhhammad@hotmail.com>
2026-03-11 22:54:29 +1100

    Added ability to count in multiple surahs for a given letter

--- check_19.py
+++ check_19.py
@@ -6,20 +6,38 @@ def divisible_by_19(n):
     return n % 19 == 0
 
 
-if len(sys.argv)!=3:
-    print("Usage: python3 check_19.py <surah> <letter>")
+if len(sys.argv) < 3:
+    print("Usage: python3 check_19.py <letter> <surah1> [surah2] [surah3] ...")
     sys.exit()
 
-surah=int(sys.argv[1])
-letter=sys.argv[2]
+letter = sys.argv[1]
+surah_numbers = [int(s) for s in sys.argv[2:]]
 
-plain,uthmani=count_letter_in_surah(surah,letter)
+total_plain = 0
+total_uthmani = 0
 
-print("Surah:",surah)
-print("Letter:",letter)
+print("Letter:", letter)
+print("Surahs:", surah_numbers)
+print()
 
-print("\nPlain count:",plain)
-print("Divisible by 19:",divisible_by_19(plain))
+for surah in surah_numbers:
 
-print("\nUthmani count:",uthmani)
-print("Divisible by 19:",divisible_by_19(uthmani))
+    plain, uthmani = count_letter_in_surah(surah, letter)
+
+    total_plain += plain
+    total_uthmani += uthmani
+
+    print(f"Surah {surah}")
+    print("  Plain count   :", plain)
+    print("  Uthmani count :", uthmani)
+    print()
+
+print("TOTAL COUNTS")
+print("-------------------")
+print("Plain total   :", total_plain)
+print("Divisible by 19:", divisible_by_19(total_plain))
+
+print()
+
+print("Uthmani total :", total_uthmani)
+print("Divisible by 19:", divisible_by_19(total_uthmani))
